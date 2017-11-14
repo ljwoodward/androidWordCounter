4dd4f8c3eb6f24a9aa127ea941911d8ae057a54e
Laurence <ljwoodward.code@gmail.com>
2017-11-14 09:15:50 +0000

    mvp acheived

--- app/src/main/java/codeclan/com/wordcounter/Controllers/MainActivity.java
+++ app/src/main/java/codeclan/com/wordcounter/Controllers/MainActivity.java
@@ -2,14 +2,41 @@ package codeclan.com.wordcounter.Controllers;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import codeclan.com.wordcounter.Models.WordCount;
 import codeclan.com.wordcounter.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView title;
+    private TextView instruction;
+    private TextView result;
+    private EditText editTextFront;
+    private Button totalCountButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.title = findViewById(R.id.title);
+        this.instruction = findViewById(R.id.instruction);
+        this.result = findViewById(R.id.result);
+        this.editTextFront = (EditText) findViewById(R.id.editTextFront);
+        this.totalCountButton = (Button) findViewById(R.id.totalCountButton);
+    }
+
+    public void onButtonClicked(View button) {
+        Log.d(getClass().toString(), "totalCountButtonClicked");
+        String text = this.editTextFront.getText().toString();
+        Log.d(getClass().toString(), text);
+        WordCount wordCount = new WordCount(text);
+        int answer = wordCount.countWords();
+        this.result.setText(answer + " words");
     }
 }
